c568fa7cc36620668d29493f8a2da29f3cf0c252
Charles Billette <charles@cbillette.com>
2024-07-23 09:53:21 -0400

    wip

--- rpc/convert_metadata_test.go
+++ rpc/convert_metadata_test.go
@@ -1,11 +1,9 @@
 package rpc
 
 import (
-	"fmt"
 	"testing"
 
 	firecoreRPC "github.com/streamingfast/firehose-core/rpc"
-	"github.com/streamingfast/firehose-gear/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,17 +13,8 @@ func Test_ConvertMetadata(t *testing.T) {
 	gearClients.Add(gearClient)
 
 	mc := NewMetadataConverter(gearClients, nil, nil)
-	ttypes, err := mc.Convert("")
+	_, err := mc.Convert("")
 	require.NoError(t, err)
 
-	protobufMessages := &types.ProtobufMessages{}
-
-	for key := range ttypes {
-		protobufMessages.Messages = append(protobufMessages.Messages, &types.ProtobufMessage{
-			Name: key,
-		})
-	}
-
-	fmt.Println(protobufMessages.ToProtoMessage())
 	require.True(t, false)
 }
