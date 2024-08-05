88621ec4d06a4b473a8c363a6d0429a44c9a9bda
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2024-08-05 09:00:37 -0400

    adding is boostraping log

--- rpc/fetcher.go
+++ rpc/fetcher.go
@@ -138,6 +138,7 @@ func (f *Fetcher) fetchBlockData(_ context.Context, requestedBlockNum uint64) (*
 
 		runtimeSpecVersion := f.lastBlockInfo.specVersion
 		if isBoostraping(f.metadata, f.lastBlockInfo.specVersion) { // bootstraping
+			f.logger.Info("boostraping metadata and spec version")
 			_, err := f.setMetadata(blockHash, client)
 			if err != nil {
 				return nil, fmt.Errorf("failed to update metadata: %w", err)
